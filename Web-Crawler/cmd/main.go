565dd05a2189d30c33e5d378b6bb5e6827003da6
Naveed Khosa <naveedkhosa2006@gmail.com>
2026-05-06 16:16:53 +0500

    Made a concurrent Web Crawler to handle unbounded data and recursive concurrency

---
+++ Web-Crawler/cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"web-crawler/pkg/engine"
+	"web-crawler/pkg/frontier"
+)
+
+func main() {
+	start := time.Now()
+
+	// Initialize our Layer 3 Registry
+	registry := frontier.NewRegistry()
+
+	// Config
+	seedURL := "https://golang.org" // Change this to any site you want to test
+	maxDepth := 2                   // Be careful with high numbers!
+
+	// Start the Engine
+	engine.Crawl(seedURL, maxDepth, registry)
+
+	// Final Summary
+	fmt.Printf("\n--- Final Report ---\n")
+	fmt.Printf("Unique URLs Found: %d\n", registry.Count())
+	fmt.Printf("Time Elapsed: %v\n", time.Since(start))
+}
